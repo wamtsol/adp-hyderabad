22f858314e4073a315b74fa691de611a60de2ec3
raheelsiddique66 <41738172+raheelsiddique66@users.noreply.github.com>
2021-07-31 21:34:15 +0500

    front

---
+++ index.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="shortcut icon" href="images/favicon.png" />
+<title>DDIS-Jamshoro-2020-21</title>
+<link type="text/css" rel="stylesheet" href="admin/css/font-awesome.min.css" />
+<link type="text/css" rel="stylesheet" href="admin/css/font-awesome.css" />
+<link type="text/css" rel="stylesheet"  href="admin/css/bootstrap.css" />
+<link type="text/css" rel="stylesheet"  href="style.css" />
+</head>
+<body>
+    <div class="container">
+        <div class="main">
+            <div class="header">
+                <div class="row">
+                    <div class="col-md-2">
+                        <div class="logo"><img src="images/logo.png"></div>
+                    </div>
+                    <div class="col-md-10">
+                        <div class="district">
+                            <h1>ANNUAL DEVELOPMENT PROGRAMME</h1>
+                            <h2>District Jamshoro</h2>
+                            <span class="year">2020-2021</span>
+                            
+                        </div>
+                        <div class="commisioner">
+                            <h3>DEPUTY COMMISSIONER JAMSHORO</h3>
+                            <h4>District Development Information System</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="content">
+                <div class="filters">
+                    <div class="row margin-b-10">
+                        <div class="col-md-3">
+                            <a href="#" class="btn btn-md btn-default">Click here to main page</a>
+                            <!-- <a href="#" class="btn btn-md btn-info">Summary</a> -->
+                        </div>
+                        <div class="col-md-3">
+                            <a href="#" class="btn btn-md btn-success">Sector Wise Summary On-going</a>
+                        </div>
+                        <div class="col-md-3">
+                            <a href="#" class="btn btn-md btn-info">Taluka Wise Summary On-going</a>
+                        </div>
+                        <div class="col-md-3">
+                            <a href="#" class="btn btn-md btn-warning">Detailed Report On-going</a>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-3">
+                            <a href="#" class="btn btn-md btn-default">Summary</a>
+                            <!-- <a href="#" class="btn btn-md btn-info">Summary</a> -->
+                        </div>
+                        <div class="col-md-3">
+                            <a href="#" class="btn btn-md btn-success">Sector Wise Summary New Scheme</a>
+                        </div>
+                        <div class="col-md-3">
+                            <a href="#" class="btn btn-md btn-info">Taluka Wise Summary New Scheme</a>
+                        </div>
+                        <div class="col-md-3">
+                            <a href="#" class="btn btn-md btn-warning">Detailed Report New Scheme</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="summary">
+                    <h2>Summary</h2>
+                    <h3>For District ADP 2020-2021</h3>
+                    <p>On-Going Scheme</p>
+                    <div class="summary-list">
+                        <table class="table table-responsive">
+                            <thead>
+                                <tr>
+                                    <th>Approved Cost <i class="fa fa-caret-down"></i></th>
+                                    <th>Throw Forward as on 01.07.2020 <i class="fa fa-caret-up"></i></th>
+                                    <th>No. of Scheme</th>
+                                    <th>Original Budget FY 2020-21 </th>
+                                    <th>Final  Budget FY 2020-21 </th>
+                                    <th>Prog. Releases </th>
+                                    <th>Prog. Expenditure</th>
+                                    <th>Prog Rel as % of Final Budget</th>
+                                    <th>Prog Exp as % of Final Budget</th>
+                                    <th>% Utilization of Released Amount</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>2,920.92</td>
+                                    <td>1,259.249</td>
+                                    <td>445</td>
+                                    <td>433.577</td>
+                                    <td>619.664</td>
+                                    <td>551.478</td>
+                                    <td>503.586</td>
+                                    <td>89%</td>
+                                    <td>81.27%</td>
+                                    <td>91.32%</td>
+                                </tr>
+                                <tr>
+                                    <td>2,920.92</td>
+                                    <td>1,259.249</td>
+                                    <td>445</td>
+                                    <td>433.577</td>
+                                    <td>619.664</td>
+                                    <td>551.478</td>
+                                    <td>503.586</td>
+                                    <td>89%</td>
+                                    <td>81.27%</td>
+                                    <td>91.32%</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <h2>Summary</h2>
+                    <h3>For District ADP 2020-2021</h3>
+                    <p>New Scheme</p>
+                    <div class="summary-list">
+                        <table class="table table-responsive">
+                            <thead>
+                                <tr>
+                                    <th>Approved Cost <i class="fa fa-caret-down"></i></th>
+                                    <th>Throw Forward as on 01.07.2020 <i class="fa fa-caret-up"></i></th>
+                                    <th>No. of Scheme</th>
+                                    <th>Original Budget FY 2020-21 </th>
+                                    <th>Final  Budget FY 2020-21 </th>
+                                    <th>Prog. Releases </th>
+                                    <th>Prog. Expenditure</th>
+                                    <th>Prog Rel as % of Final Budget</th>
+                                    <th>Prog Exp as % of Final Budget</th>
+                                    <th>% Utilization of Released Amount</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>2,920.92</td>
+                                    <td>1,259.249</td>
+                                    <td>445</td>
+                                    <td>433.577</td>
+                                    <td>619.664</td>
+                                    <td>551.478</td>
+                                    <td>503.586</td>
+                                    <td>89%</td>
+                                    <td>81.27%</td>
+                                    <td>91.32%</td>
+                                </tr>
+                                <tr>
+                                    <td>2,920.92</td>
+                                    <td>1,259.249</td>
+                                    <td>445</td>
+                                    <td>433.577</td>
+                                    <td>619.664</td>
+                                    <td>551.478</td>
+                                    <td>503.586</td>
+                                    <td>89%</td>
+                                    <td>81.27%</td>
+                                    <td>91.32%</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <div class="footer">
+                <div class="row">
+                    <div class="col-md-6">
+                        <p>District Development Information System - Jamshoro</p>
+                    </div>
+                    <div class="col-md-6">
+                        <p class="text-right">Designed & Maintained By:- Wamtsol.com</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
